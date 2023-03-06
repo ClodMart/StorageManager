28914dcb117474fc730b0d51e4c307d68f364e45
Claudio Martinoli <claudio@axiotek.eu>
2023-03-06 08:12:20 +0100

    servizio

--- DBManager/Interfacce/DrinkIngredientsRepository.cs
+++ DBManager/Interfacce/DrinkIngredientsRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,39 +11,68 @@ namespace DBManager.Interfacce
 {
     public class DrinkIngredientsRepository : IRepository<DrinkIngredient>
     {
-            private readonly GestioneMagazzinoContext _dbContext;
+        private readonly GestioneMagazzinoContext _dbContext;
 
-            public DrinkIngredientsRepository(GestioneMagazzinoContext dbContext)
-            {
-                _dbContext = dbContext;
-            }
+        public DrinkIngredientsRepository(GestioneMagazzinoContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
-            public IEnumerable<DrinkIngredient> GetAll()
-            {
-                return _dbContext.DrinkIngredients.ToList();
-            }
+        public IEnumerable<DrinkIngredient> GetAll()
+        {
+            return _dbContext.DrinkIngredients.ToList();
+        }
 
-            public DrinkIngredient GetById(int id)
-            {
-                return _dbContext.DrinkIngredients.Find(id);
-            }
+        public DrinkIngredient GetById(int id)
+        {
+            return _dbContext.DrinkIngredients.Find(id);
+        }
 
-            public void Add(DrinkIngredient entity)
-            {
-                _dbContext.DrinkIngredients.Add(entity);
-                _dbContext.SaveChanges();
-            }
+        public void Add(DrinkIngredient entity)
+        {
+            _dbContext.DrinkIngredients.Add(entity);
+            _dbContext.SaveChanges();
+        }
 
-            public void Update(DrinkIngredient entity)
-            {
-                _dbContext.Entry(entity).State = EntityState.Modified;
-                _dbContext.SaveChanges();
-            }
+        public void Update(DrinkIngredient entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
 
-            public void Delete(DrinkIngredient entity)
+        public void Delete(DrinkIngredient entity)
+        {
+            _dbContext.DrinkIngredients.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void InsertFromCSV(string fileUri)
+        {
+            using (var reader = new StreamReader(fileUri))
             {
-                _dbContext.DrinkIngredients.Remove(entity);
-                _dbContext.SaveChanges();
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    DrinkIngredient myObject = new DrinkIngredient();
+
+                    myObject.DrinkName = values[0];
+                    myObject.Category = values[1];
+                    myObject.IsUsed = int.Parse(values[2]);
+                    myObject.SupplierId = int.Parse(values[3]);
+                    myObject.Cost = decimal.Parse(values[4]);
+                    myObject.SizeLiters = decimal.Parse(values[5]);
+                    myObject.SizeUnits = decimal.Parse(values[6]);
+                    //myObject.CostLiter= decimal.Parse(values[7]);
+                    //myObject.CostUnit= decimal.Parse(values[8]);
+                    myObject.QuantityNeeded = int.Parse(values[7]);
+                    myObject.Notes = values[8];
+
+                    this.Add(myObject);
+                }
+
             }
+        }
     }
 }
--- DBManager/Models/GestioneMagazzinoContext.cs
+++ DBManager/Models/GestioneMagazzinoContext.cs
@@ -28,12 +28,14 @@ namespace DBManager.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GestioneMagazzino;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=GestioneMagazzino;Integrated Security=True");
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");
+
             modelBuilder.Entity<DrinkIngredient>(entity =>
             {
                 entity.ToTable("Drink_Ingredients");
