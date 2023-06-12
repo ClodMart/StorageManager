633bc09af2ec60719d90de26ad0a555eafd26159
Claudio Martinoli <claudiomartinoli96@gmai.com>
2023-06-12 19:50:18 +0200

    Commit Servizio

--- DBManager/Models/StorageManagerDBContext.cs
+++ DBManager/Models/StorageManagerDBContext.cs
@@ -34,9 +34,10 @@ namespace DBManager.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=192.168.1.107,5432;Database=StorageManagerDB;Username=postgres;Password=1234");
+                optionsBuilder.UseNpgsql("Host=10.147.18.219,5432;Database=StorageManagerDB;Username=StorageManagerAPP;Password=StorageManagerAPP");
             }
         }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
